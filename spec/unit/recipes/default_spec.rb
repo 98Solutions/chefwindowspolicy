86f409ae24eb584ae061141a95e582de627878b9
Allan Bernardo (Avanade) <allan.bernardo@gatesfoundation.org>
2021-02-17 14:25:34 -0800

    Adding more steps to drone pipeline
    Signed-off-by: Allan Bernardo (Avanade) <allan.bernardo@gatesfoundation.org>

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -6,21 +6,18 @@
 
 require 'spec_helper'
 
-describe 'winstacks::default' do
-  context 'When all attributes are default, on Ubuntu 20.04' do
-    # for a complete list of available platforms and versions see:
-    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'ubuntu', '20.04'
-
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+describe 'winstacks' do
+  context 'When all attributes are default, on an windows' do
+    before do
+      stub_const('Win32::Service', Module.new) unless defined?(Win32::Service)
+      allow(Win32::Service).to receive(:exists?).and_return(true)
     end
-  end
-
-  context 'When all attributes are default, on CentOS 8' do
-    # for a complete list of available platforms and versions see:
-    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'centos', '8'
+    let(:chef_run) do
+      # for a complete list of available platforms and versions see:
+      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
+      runner = ChefSpec::ServerRunner.new(platform: 'windows', version: '10')
+      runner.converge(described_recipe)
+    end
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
